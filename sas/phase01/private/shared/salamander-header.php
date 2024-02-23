c48c2310b200815e34d052bbcdc1533cc0f09798
jeremyw0886 <125689857+jeremyw0886@users.noreply.github.com>
2024-02-22 21:44:54 -0500

    Adds files for phase01 assignment

---
+++ sas/phase01/private/shared/salamander-header.php
@@ -0,0 +1,11 @@
+<?php if(!isset($pageTitle)) { $pageTitle = 'Salamanders'; } ?>
+
+    <header>
+      <h1><a href="<?= urlFor('/'); ?>">Southern Appalachian Salamanders (SAS)</a></h1>
+    </header>
+    <navigation>
+      <ul>
+      <li><a href="<?= urlFor('salamanders/'); ?>">Salamanders</a></li>
+      </ul>
+    </navigation>
+
