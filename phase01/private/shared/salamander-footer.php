d74894a59f1c0af61d4ae39b7c361b8dc08a438a
Jeremy Warren <jeremyw0815@icloud.com>
2024-02-22 21:20:10 -0500

    Code for phase01 is ready

---
+++ phase01/private/shared/salamander-footer.php
@@ -0,0 +1,6 @@
+<footer>
+  <p>&copy; <?= date('Y'); ?> Southern Appalachian Salamanders</p><br>
+  <a href="../../../../index.html">Home</a>
+</footer>
+
+<!--  -->
---
+++ phase01/public/salamanders/show.php
@@ -0,0 +1,22 @@
+<?php require_once('../../private/initialize.php'); $id = $_GET['id'] ?? 1; $pageTitle = 'Salamander Details'; include(SHARED_PATH . '/salamander-header.php'); ?> 
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>SAS</title>
+      <!-- Add any other meta tags and links to CSS files here -->
+  </head>
+  <body>
+
+  <?php include('../private/shared/salamander-header.php'); ?>
+
+  <h2>Salamander Details</h2>
+  <p>Page ID: <?= h($id) ?></p>
+  <p><a href="<?= urlFor('/salamanders/index.php'); ?>">&laquo; Back to Salamander List</a></p>
+
+  <?php include(SHARED_PATH . '/salamander-footer.php'); ?>
+
+  </body>
+</html>
