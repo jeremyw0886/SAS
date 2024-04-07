1fca6fa1a42aebebf38977a517a10a2ecb7534af
jeremyw0886 <jeremyw0815@icloud.com>
2024-04-07 14:11:30 -0400

    Phase05 Complete

---
+++ phase05/private/db_credentials.php
@@ -0,0 +1,18 @@
+<?php
+// local
+// define("DB_SERVER", "localhost");
+// define("DB_USER", "sally");
+// define("DB_PASS", "somepa55word");
+// define("DB_NAME", "salamanders");
+
+//a2
+// define("DB_SERVER", "localhost");
+// define("DB_USER", "charli12_doall");
+// define("DB_PASS", "ErYje7HV2o2Lqp7B9gx");
+// define("DB_NAME", "charli12_salamanders");
+
+
+define('DB_SERVER', 'localhost');
+define('DB_USER', 'czpshnmy_sally');
+define('DB_PASS', 'somePa55word');
+define('DB_NAME', 'czpshnmy_salamanders');
